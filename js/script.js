c547753df39c9af51f25b136594b0040891aaf04
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-04-01 20:30:17 -0400

    Added the code to replace existing html
    Used .innerHTML to replace the existing html each time the getRandomQuote funcion is called.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 
 //Array that is storing all of the quotes and sources
 var quotes = [
@@ -27,3 +25,28 @@ var quotes = [
 		source: "Albert Einstein"		
 	}
 ]
+
+
+//selects a random quote object from the quotes array
+// returns the randomly selected quote object
+function getRandomQuote() {
+	var randomNum = Math.floor(Math.random() * 5);
+	for (var i = 0; i < quotes.length; i++ ) {
+		if (quotes[i] == quotes[randomNum]) {
+			return quotes[i];
+		}
+	}
+};
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+//innerHTML changes the selected elements and replaces the content with the randomly generated quote
+function printQuote() {
+	var randQuote = getRandomQuote();
+	document.querySelector('.quote').innerHTML = randQuote.quote;
+	document.querySelector('.source').innerHTML = randQuote.source;
+};
+	
